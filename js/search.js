319a77ac1619b171b10bd8be02571d799b55566a
fiziorh <fizio69@gmail.com>
2021-08-04 00:02:35 +0700

    added some description

--- js/search.js
+++ js/search.js
@@ -1,15 +1,23 @@
+
+// first, make a function declaration
 function search_gallery() {
+    // the input variable get the value of what you type on the searchbar
     let input = document.getElementById('searchbar').value;
+    // make it toLowerCase for uppercase sensitivity
     input = input.toLowerCase();
+    // the x variable gather the anchor tags information
     let x = document.getElementsByTagName('a');
 
-
+    // make a loop that compare it to the length of "x variable" 
     for (i = 0; i < x.length; i++) {
 
+        // the caption variable get the data-caption attribute from the anchor tags
         let caption = x[i].getAttribute('data-caption').toLowerCase();
+        // if the caption variable doesn't match the input (what you type on the searchbar), then it displays "none"
         if (!caption.includes(input)) {
             x[i].style.display = "none";
         }
+        // else it will display it
         else {
             x[i].style.display = "flex"
         }
