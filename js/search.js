7df9211b647537e4218bf3c7c9f8e7ef62800d2c
fiziorh <fizio69@gmail.com>
2021-08-03 21:56:18 +0700

    fixing the search

--- js/search.js
+++ js/search.js
@@ -1,30 +1,31 @@
-function search_animal() {
+function search_gallery() {
     let input = document.getElementById('searchbar').value;
     input = input.toLowerCase();
-    let x = document.getElementsByClassName('items');
+    let x = document.getElementsByTagName('a');
 
 
     for (i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().include(input)) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display = "none";
         }
         else {
-            x[i].style.display = "list.item"
+            x[i].style.display = "flex"
         }
 
     }
 }
 
 
-function search() {
+// function search() {
 
-    //VARIABLES
-    let input = document.getElementById('searchbar');
-    let filter = input.value.toUpperCase();
-    let gallery = document.getElementById('all');
-    let data = a.getElemetsByTagName('data-caption');
+//     //VARIABLES
+//     let input = document.getElementById('searchbar');
+//     let filter = input.value.toUpperCase();
+//     let contain = document.getElementById('all');
+//     let data = contain.getElemetsByTagName('data-caption');
 
-    //LOOP
-    for (i = 0; i < data.length; i++) {
-        a = li[i]
-    }
+//     //LOOP
+//     for (i = 0; i < data.length; i++) {
+//         a = li[i]
+//     }
+// }
